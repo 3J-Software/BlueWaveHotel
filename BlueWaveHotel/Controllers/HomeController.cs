135ca1170a7b987bec71dce0b6b466d3bc7f9f13
sefadolu <sefaa.doluu@gmail.com>
2024-09-17 20:14:58 +0300

    About and policy pages created

--- BlueWaveHotel/Controllers/HomeController.cs
+++ BlueWaveHotel/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace BlueWaveHotel.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Policy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
