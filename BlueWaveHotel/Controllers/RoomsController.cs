94f6ea002b94d694c193945d0a5c72ea0f1a17f8
sefadolu <sefaa.doluu@gmail.com>
2024-09-23 14:52:54 +0300

    rooms create

---
+++ BlueWaveHotel/Controllers/RoomsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlueWaveHotel.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    return View("Details1");
+                case 2:
+                    return View("Details2");
+                case 3:
+                    return View("Details3");
+                case 4:
+                    return View("Details4");
+                case 5:
+                    return View("Details5");
+                default:
+                    return NotFound();
+            }
+        }
+    }
+}
