daff64aa3e927e7491831eeef0da4187df3ef6e6
Serdar akdemir <131920543+LOCKDOWNTDS@users.noreply.github.com>
2024-10-03 20:31:15 +0300

    Reservation updated By Serdar

--- BlueWaveHotel/Controllers/ReservationController.cs
+++ BlueWaveHotel/Controllers/ReservationController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlueWaveHotel.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlueWaveHotel.Controllers
 {
-    public class ReservationController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class ReservationController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index(DateTime TimeNow, int id)
+		{
+			int Days = nextMonth(TimeNow, id);
+			var model = new ReservationViewModel()
+			{
+				Days = Days,
+			};
+			return View(model);
+		}
+
+		int nextMonth(DateTime TimeNow, int BackOrNext)
+		{
+			var Year = TimeNow.Year;
+			var Month = TimeNow.Month;
+			if (BackOrNext == 1)
+			{
+				// int monthDiff = ((DateTime.Now.AddMonths(3).Year - DateTime.Now.Year) * 12) + 
+				// DateTime.Now.AddMonths(3).Month - DateTime.Now.Month;
+
+				if (Month == 12)
+				{
+					Month = 1;
+					Year = +1;
+				}
+				else
+				{
+					Month = +1;
+				}
+				return DateTime.DaysInMonth(Year, Month);
+			}
+			else if (BackOrNext == -1)
+			{
+				if (Month == 1)
+				{
+					Month = 12;
+					Year = -1;
+				}
+				else
+				{
+					Month = +1;
+				}
+				return DateTime.DaysInMonth(Year, Month);
+			}
+			return DateTime.DaysInMonth(Year, Month);
+		}
+	}
 }
---
+++ BlueWaveHotel/Models/ReservationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlueWaveHotel.Models
+{
+	public class ReservationViewModel
+	{
+		public int Days { get; set; }
+		public string Month { get; set; }
+	}
+}
