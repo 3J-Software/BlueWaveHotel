5113b9557ae7dc63b9dcaee49a93185e5d512f05
Serdar akdemir <131920543+LOCKDOWNTDS@users.noreply.github.com>
2024-10-08 17:13:25 +0300

    Month in Callender  completed

--- BlueWaveHotel/Controllers/ReservationController.cs
+++ BlueWaveHotel/Controllers/ReservationController.cs
@@ -5,34 +5,57 @@ namespace BlueWaveHotel.Controllers
 {
 	public class ReservationController : Controller
 	{
+		public DateTime TimeNow = DateTime.Now;
 		[HttpGet]
-		public IActionResult Index(DateTime TimeNow, int id)
+		public IActionResult Index(int id)
 		{
-			int Days = nextMonth(TimeNow, id);
+			int Days = DaysInnextMonth(TimeNow, id);
+			DateTime month = nextMonth(TimeNow, 3);
+			string currentMonth = month.ToString("MMMM");
+			int currentYear = month.Year;
 			var model = new ReservationViewModel()
 			{
 				Days = Days,
+				ResultTime = month,
+				Month = currentMonth,
+				Year = currentYear
+			};
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public IActionResult Index(DateTime TimeNow, int id)
+		{
+			int Days = DaysInnextMonth(TimeNow, id);
+			DateTime month = nextMonth(TimeNow, id);
+			string currentMonth = month.ToString("MMMM");
+			int currentYear = month.Year;
+			var model = new ReservationViewModel()
+			{
+				Days = Days,
+				ResultTime = month,
+				Month = currentMonth,
+				Year = currentYear
 			};
 			return View(model);
 		}
 
-		int nextMonth(DateTime TimeNow, int BackOrNext)
+		int DaysInnextMonth(DateTime TimeNow, int BackOrNext)
 		{
 			var Year = TimeNow.Year;
 			var Month = TimeNow.Month;
 			if (BackOrNext == 1)
 			{
-				// int monthDiff = ((DateTime.Now.AddMonths(3).Year - DateTime.Now.Year) * 12) + 
-				// DateTime.Now.AddMonths(3).Month - DateTime.Now.Month;
 
 				if (Month == 12)
 				{
 					Month = 1;
-					Year = +1;
+					Year += 1;
 				}
 				else
 				{
-					Month = +1;
+					Month += 1;
 				}
 				return DateTime.DaysInMonth(Year, Month);
 			}
@@ -41,15 +64,28 @@ namespace BlueWaveHotel.Controllers
 				if (Month == 1)
 				{
 					Month = 12;
-					Year = -1;
+					Year -= 1;
 				}
 				else
 				{
-					Month = +1;
+					Month -= 1;
 				}
 				return DateTime.DaysInMonth(Year, Month);
 			}
 			return DateTime.DaysInMonth(Year, Month);
+		}
+
+		DateTime nextMonth(DateTime TimeNow, int BackOrNext)
+		{
+			if (BackOrNext == 1)
+			{
+				return TimeNow.AddMonths(1);
+			}
+			else if (BackOrNext == -1)
+			{
+				return TimeNow.AddMonths(-1);
+			}
+			return TimeNow;
 		}
 	}
 }
--- BlueWaveHotel/Models/ReservationViewModel.cs
+++ BlueWaveHotel/Models/ReservationViewModel.cs
@@ -4,5 +4,8 @@
 	{
 		public int Days { get; set; }
 		public string Month { get; set; }
+		public int Year { get; set; }
+		public DateTime TimeNow { get; set; }
+		public DateTime ResultTime { get; set; }
 	}
 }
